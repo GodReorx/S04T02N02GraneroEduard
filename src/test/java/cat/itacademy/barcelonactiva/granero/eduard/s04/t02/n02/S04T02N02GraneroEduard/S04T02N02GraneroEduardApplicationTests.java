27d361fe533808aebe85686f5a7799eed862cd35
Eduard Granero <155961655+GodReorx@users.noreply.github.com>
2024-04-09 10:32:43 +0200

    Add files via upload
    Creacion inicial

---
+++ src/test/java/cat/itacademy/barcelonactiva/granero/eduard/s04/t02/n02/S04T02N02GraneroEduard/S04T02N02GraneroEduardApplicationTests.java
@@ -0,0 +1,13 @@
+package cat.itacademy.barcelonactiva.granero.eduard.s04.t02.n02.S04T02N02GraneroEduard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class S04T02N02GraneroEduardApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
